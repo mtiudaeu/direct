c9d6a269b226d7ad0ee0cb133134adb3816e5e89
Mathieu Drapeau <m.drapeau9@gmail.com>
2022-09-14 17:26:38 -0400

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -26,26 +26,43 @@ HRESULT Menu::onRender(ID2D1HwndRenderTarget* pRenderTarget, IDWriteTextFormat* 
 
     static const WCHAR sc_welcome[] = L"Welcome!";
     static const WCHAR sc_tetris[] = L"Tetris";
+    static const WCHAR sc_snake[] = L"Snake";
 
     D2D1_SIZE_F renderTargetSize = pRenderTarget->GetSize();
+    D2D1::Matrix3x2F transform;
     
-    pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
-    pRenderTarget->SetTransform(D2D1::Matrix3x2F::Translation(0, -100));
+    //--------------------
+    transform = D2D1::Matrix3x2F::Translation(0, -(renderTargetSize.height / 10));
+    pRenderTarget->SetTransform(transform);
     pRenderTarget->DrawText(
         sc_welcome,
         ARRAYSIZE(sc_welcome) - 1,
         pTextFormat,
-        D2D1::RectF(0, 0, renderTargetSize.width, renderTargetSize.height),
+        D2D1::RectF(0, 0, renderTargetSize.width/2, renderTargetSize.height/2),
         m_pBlackBrush
     );
     
-
-    pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
-    pRenderTarget->SetTransform(D2D1::Matrix3x2F::Translation(0, 100));
-    //pRenderTarget->SetTransform(D2D1::Matrix3x2F::Scale(0.5f, 0.5f));
+    //--------------------
+    transform = D2D1::Matrix3x2F::Scale(0.5f, 0.5f) 
+        * D2D1::Matrix3x2F::Translation((renderTargetSize.width / 4), (renderTargetSize.height / 4))
+        * D2D1::Matrix3x2F::Translation(0, (renderTargetSize.height / 8));
+    pRenderTarget->SetTransform(transform);
     pRenderTarget->DrawText(
         sc_tetris,
         ARRAYSIZE(sc_tetris) - 1,
+        pTextFormat,
+        D2D1::RectF(0, 0, renderTargetSize.width, renderTargetSize.height),
+        m_pBlackBrush
+    );
+
+    //--------------------
+    transform = D2D1::Matrix3x2F::Scale(0.5f, 0.5f)
+        * D2D1::Matrix3x2F::Translation((renderTargetSize.width / 4), (renderTargetSize.height / 4))
+        * D2D1::Matrix3x2F::Translation(0, (2 * renderTargetSize.height / 8));
+    pRenderTarget->SetTransform(transform);
+    pRenderTarget->DrawText(
+        sc_snake,
+        ARRAYSIZE(sc_snake) - 1,
         pTextFormat,
         D2D1::RectF(0, 0, renderTargetSize.width, renderTargetSize.height),
         m_pBlackBrush
