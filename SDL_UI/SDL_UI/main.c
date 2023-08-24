b3b356a4a7df1c4bbf2c65da5c3f0fdb79bc4f15
Mathieu Drapeau <m.drapeau9@gmail.com>
2023-08-24 14:02:10 -0400

    WIP

--- SDL_UI/SDL_UI/main.c
+++ SDL_UI/SDL_UI/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+
+#include <SDL2/SDL_ttf.h>
 
 #define SCREEN_WIDTH   1280
 #define SCREEN_HEIGHT  720
@@ -9,11 +13,17 @@
 typedef struct {
 	SDL_Renderer *renderer;
 	SDL_Window *window;
+	TTF_Font* font;
+	SDL_Surface* text;
+	SDL_Color white;
+	SDL_Texture* caption;
+	SDL_FRect captionRect;
 } App;
 
 App app;
+bool shouldExit = false;
 
-void initSDL(void)
+void init(void)
 {
 	int rendererFlags, windowFlags;
 
@@ -44,6 +54,36 @@ void initSDL(void)
 		printf("Failed to create renderer: %s\n", SDL_GetError());
 		exit(1);
 	}
+
+	//app.white = { 0xFF, 0xFF, 0xFF, 0 };
+
+	if (TTF_Init() < 0)
+	{
+		printf("Couldn't initialize SDL TTF: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+
+	const int font_size = 12;
+	app.font = TTF_OpenFont("OpenSans.ttf", font_size);
+	app.text = TTF_RenderUTF8_Solid(app.font, "test message", app.white);
+	app.captionRect.x = 4.0f;
+	app.captionRect.y = 4.0f;
+	app.captionRect.w = (float)app.text->w;
+	app.captionRect.h = (float)app.text->h;
+	app.caption = SDL_CreateTextureFromSurface(app.renderer, app.text);
+	//mdtmp SDL_DestroySurface(app.text);
+}
+
+void uninit(void)
+{
+	TTF_CloseFont(app.font);
+	TTF_Quit();
+
+	SDL_DestroyRenderer(app.renderer);
+	SDL_DestroyWindow(app.window);
+
+	SDL_Quit();
 }
 
 void doInput(void)
@@ -55,7 +95,7 @@ void doInput(void)
 		switch (event.type)
 		{
 			case SDL_QUIT:
-				exit(0);
+				shouldExit = true;
 				break;
 
 			default:
@@ -72,6 +112,8 @@ void prepareScene(void)
 
 void presentScene(void)
 {
+
+	SDL_RenderCopy(app.renderer, app.caption, NULL, &app.captionRect);
 	SDL_RenderPresent(app.renderer);
 }
 
@@ -79,9 +121,9 @@ int WinMain(int argc, char *argv[])
 {
 	memset(&app, 0, sizeof(App));
 
-	initSDL();
+	init();
 
-	while (1)
+	while (!shouldExit)
 	{
 		prepareScene();
 
@@ -91,6 +133,8 @@ int WinMain(int argc, char *argv[])
 
 		SDL_Delay(16);
 	}
+
+	uninit();
 
 	return 0;
 }
